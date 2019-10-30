ee5a4bc81432819f9fcb3cfc1b6ac27bbdb3ffca
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-30 15:46:16 +0000

    working on GET and POST requests

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,7 +8,13 @@ const router = express.Router();
 
 // POST requests
 router.post('/', (req, res) => {
-
+  db.insert(req.body)
+  .then(data => {
+    res.status(201).json(data);
+  })
+  .catch(error => {
+    res.status(400).json({errorMessage: "Please provide a name for the post."})
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -27,11 +33,25 @@ router.get('', middle.logger, (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', middle.logger, (req, res) => {
+  db.getById(req.params.id)
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'The post information could not be retrieved.'})
+  })
 
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', middle.logger, (req, res) => {
+  db.getUserPosts(req.params.id)
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'The posts information could not be retrieved.'})
+  })
 
 });
 
